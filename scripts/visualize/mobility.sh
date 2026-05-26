005d57a50830f540dac883fee5cb518297c6dac4
AiramL <airam@poli.ufrj.br>
2026-05-26 16:39:43 +0000

    new notebook implementation

--- scripts/visualize/mobility.sh
+++ scripts/visualize/mobility.sh
@@ -1,9 +1,7 @@
 execution=0
-cars=$(yq '.simulation.cars' "config/config.yaml")
-speeds=$(yq '.simulation.speed.index[]' "config/config.yaml") 
+cars=$(yq '.simulation.cars' "configs/config.yaml")
+speeds=$(yq '.simulation.speed.index[]' "configs/config.yaml") 
 
 speed=${speeds[0]}
 
-python -m utils.process.poi
-
-sumo-gui -n mobility/raw/scenarios/$speed/manhattan_net_2.xml -r mobility/raw/scenarios/$speed/Krauss/$cars/flows_file_Krauss_"$cars"_"$execution".xml -a mobility/map/poi.xml,mobility/raw/scenarios/$speed/Krauss/$cars/manhatan_rerouter_Krauss_100_0.add.xml
+sumo-gui -n mobility/raw/scenarios/$speed/manhattan_net_2.xml -r mobility/raw/scenarios/$speed/Krauss/$cars/flows_file_Krauss_"$cars"_"$execution".xml -a mobility/raw/scenarios/$speed/Krauss/$cars/manhatan_rerouter_Krauss_"$cars"_0.add.xml
