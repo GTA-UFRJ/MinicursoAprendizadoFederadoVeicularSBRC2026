a1cf587187d903a70439f4325d45708c36f7667c
AiramL <airam@poli.ufrj.br>
2026-05-26 16:34:46 +0000

    removing ip

--- scripts/run/jupyter.sh
+++ scripts/run/jupyter.sh
@@ -1,6 +1,8 @@
-ip=$(ip -4 addr show | grep inet | grep -v "127.0.0.1" | awk '{print $2}' | cut -d/ -f1 | head -n 1)
+#ip=$(ip -4 addr show | grep inet | grep -v "127.0.0.1" | awk '{print $2}' | cut -d/ -f1 | head -n 1)
 
-echo "starting jupyter notebook on ip "$ip
+ip="127.0.0.1"
+
+echo "starting jupyter notebook on ip $ip"
 
 conda activate afv_sbrc_2026
 python -m jupyterlab --ip="$ip"
